6e36e440c99e87e7912e1c57239af6140917b90c
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-09-21 15:50:57 +0200

    Refactor train_xgb_tree.py
    - Remove unused imports
    - Reformat with Black
    - Add ArgumentParser for input and output directory
    - Make train_xgb_tree.py executable
    - Move code for finding frame directories to function
    - Remove ipdb
    - Use xgboost's methods for saving/loading the model.  This should make
      it easier to load the model in C++.
    - Refactor train_xgb_tree.py a bit

--- scripts/train_xgb_tree.py
+++ scripts/train_xgb_tree.py
@@ -1,47 +1,39 @@
+#!/usr/bin/env python3
 # %%
+import argparse
 import os
 import cv2 as cv
 import pickle
-import matplotlib.pyplot as plt
-import ipdb
 import numpy as np
 import random
-import itertools
 
 import sys
 import time
 
-
 import pandas as pd
 from PIL import Image
 import traceback
-import xgboost as xgb
-from xgboost import XGBRegressor
 from xgboost import XGBClassifier
-
-from sklearn.metrics import mean_absolute_error
-
-from bidict import bidict
 
 from matplotlib import colors
 
 filename2class = {}
-filename2class['n'] = 'background'
-filename2class['y'] = 'yellow'
-filename2class['r'] = 'red'
-filename2class['m'] = 'magenta'
-filename2class['b'] = 'blue'
-filename2class['c'] = 'cyan'
-filename2class['g'] = 'green'
+filename2class["n"] = "background"
+filename2class["y"] = "yellow"
+filename2class["r"] = "red"
+filename2class["m"] = "magenta"
+filename2class["b"] = "blue"
+filename2class["c"] = "cyan"
+filename2class["g"] = "green"
 
 
 def class2bgr(pixel_class):
-    if pixel_class == 'background':
-        color = 'black'
+    if pixel_class == "background":
+        color = "black"
     else:
         color = pixel_class
     rgb = np.array(colors.to_rgba(color)[:3])
-    rgb = (rgb * 255).astype('uint8')
+    rgb = (rgb * 255).astype("uint8")
 
     return rgb[::-1]
 
@@ -54,20 +46,20 @@ def compute_identical_fraction(a, b):
 
 
 def show_image_bgr(image):
-    image = Image.fromarray(image[:, :, ::-1], 'RGB')
+    image = Image.fromarray(image[:, :, ::-1], "RGB")
     image.show()
 
 
 def create_channels(image_bgr):
     conversions = {
-        'hsv': cv.COLOR_BGR2HSV,
-        'hls': cv.COLOR_BGR2HLS,
-        'xyz': cv.COLOR_BGR2XYZ,
-        'LAB': cv.COLOR_BGR2Lab,
-        'LUV': cv.COLOR_BGR2Luv,
+        "hsv": cv.COLOR_BGR2HSV,
+        "hls": cv.COLOR_BGR2HLS,
+        "xyz": cv.COLOR_BGR2XYZ,
+        "LAB": cv.COLOR_BGR2Lab,
+        "LUV": cv.COLOR_BGR2Luv,
     }
 
-    channels = {'bgr'[i]: image_bgr[:, :, i] for i in range(3)}
+    channels = {"bgr"[i]: image_bgr[:, :, i] for i in range(3)}
 
     for key in conversions:
         image = cv.cvtColor(image_bgr, conversions[key])
@@ -77,9 +69,7 @@ def create_channels(image_bgr):
     return channels
 
 
-def create_features(*,
-                    image_bgr,
-                    flatten=False):
+def create_features(*, image_bgr, flatten=False):
 
     image_bgr = cv.medianBlur(image_bgr, 7)
     channels = create_channels(image_bgr=image_bgr)
@@ -90,125 +80,127 @@ def create_features(*,
     return channels, image_bgr.shape[:2]
 
 
-def load_segment(*,
-                 path,
-                 name):
+def load_segment(*, path, name):
 
-    image = cv.imread(os.path.join(path, 'camera' + name[1:]))
+    image = cv.imread(os.path.join(path, "camera" + name[1:]))
     features, shape = create_features(image_bgr=image, flatten=True)
     data = pd.DataFrame(features)
 
     mask = cv.imread(os.path.join(path, name))
     mask = mask.sum(axis=2) != 0
-    data['mask'] = mask.flatten()
-    data = data[data['mask'] == True]
+    data["mask"] = mask.flatten()
+    data = data[data["mask"] == True]
 
-    data['class'] = filename2class[name[0]]
+    data["class"] = filename2class[name[0]]
 
     return data
 
 
-def balance_classes(*,
-                    data,
-                    background_ratio,
-                    random_state):
-    foreground = data[data['class'] != 'background']
+def balance_classes(*, data, background_ratio, random_state):
+    foreground = data[data["class"] != "background"]
 
-    min_class_size = foreground['class'].value_counts().min()
-    foreground = foreground.groupby('class').apply(
-        lambda d: d.sample(min_class_size,
-                           random_state=random_state))
+    min_class_size = foreground["class"].value_counts().min()
+    foreground = foreground.groupby("class").apply(
+        lambda d: d.sample(min_class_size, random_state=random_state)
+    )
     foreground = foreground.reset_index(drop=True)
 
-    background = data[data['class'] == 'background']
+    background = data[data["class"] == "background"]
     n_background_points = int(background_ratio * foreground.shape[0])
-    background = background.sample(n_background_points,
-                                   random_state=random_state)
+    background = background.sample(
+        n_background_points, random_state=random_state
+    )
 
     return pd.concat([foreground, background])
 
 
-def load_images_and_create_data(*,
-                                input_path,
-                                output_filename):
+def get_frame_folders(input_path):
+    frame_folders = [
+        f
+        for f in os.listdir(input_path)
+        if os.path.isdir(os.path.join(input_path, f))
+    ]
+    return frame_folders
+
+
+def load_images_and_create_data(*, input_path, output_filename):
     # go through the folders and load all the annotated images
     # then compute features and create a pandas frame
 
     print("loading images")
 
-    frame_folders = [f for f in os.listdir(
-        input_path) if os.path.isdir(os.path.join(input_path, f))]
+    frame_folders = get_frame_folders(input_path)
 
     data = []
     for frame_folder in frame_folders:
         frame_path = os.path.join(input_path, frame_folder)
-        segment_names = [name for name in os.listdir(
-            frame_path) if name[1].isdigit()]
+        segment_names = [
+            name for name in os.listdir(frame_path) if name[1].isdigit()
+        ]
 
         if len(segment_names) == 0:
             continue
 
         for segment_name in segment_names:
             segment_data = load_segment(path=frame_path, name=segment_name)
-            segment_data['frame'] = frame_folder
+            segment_data["frame"] = frame_folder
             data += [segment_data]
 
-    data = pd.concat(data, axis='index')
+    data = pd.concat(data, axis="index")
 
     data.to_pickle(output_filename)
 
     print("done loading images")
 
 
-def prepare_data(*,
-                 data,
-                 feature_names,
-                 train_fraction,
-                 background_ratio,
-                 seed):
+def prepare_data(
+    *, data, feature_names, train_fraction, background_ratio, seed
+):
     # create training and test data from entire data frame
 
     # we split according to frames, not single pixels
     # to properly test generalization to other frames
-    frames = sorted(list(set(data['frame'])))
+    frames = sorted(list(set(data["frame"])))
     random.seed(seed)
     random.shuffle(frames)
     n_train_frames = int(len(frames) * train_fraction)
     train_frames = frames[:n_train_frames]
     test_frames = frames[n_train_frames:]
 
-    train_set = data.loc[data['frame'].isin(train_frames)]
-    train_set = balance_classes(data=train_set,
-                                background_ratio=background_ratio,
-                                random_state=seed)
+    train_set = data.loc[data["frame"].isin(train_frames)]
+    train_set = balance_classes(
+        data=train_set, background_ratio=background_ratio, random_state=seed
+    )
     train_set = train_set.sample(frac=1, random_state=seed)
 
-    test_set = data.loc[data['frame'].isin(test_frames)]
-    test_set = balance_classes(data=test_set,
-                               background_ratio=background_ratio,
-                               random_state=seed)
+    test_set = data.loc[data["frame"].isin(test_frames)]
+    test_set = balance_classes(
+        data=test_set, background_ratio=background_ratio, random_state=seed
+    )
     test_set = test_set.sample(frac=1, random_state=seed)
 
-    target = 'class'
+    target = "class"
     X_train = train_set[feature_names]
     y_train = train_set[target]
     X_test = test_set[feature_names]
     y_test = test_set[target]
 
-    assert not set(train_set['frame']).intersection(set(test_set['frame']))
-    print(train_set['class'].value_counts())
-    print(test_set['class'].value_counts())
+    assert not set(train_set["frame"]).intersection(set(test_set["frame"]))
+    print(train_set["class"].value_counts())
+    print(test_set["class"].value_counts())
 
     return X_train, y_train, X_test, y_test
 
 
 def fit_model(X_train, y_train, seed=42):
 
-    model = XGBClassifier(learning_rate=1.0,
-                          n_estimators=1,  # only one tree
-                          n_jobs=8,
-                          max_depth=6,  # maximum tree depth
-                          random_state=seed)
+    model = XGBClassifier(
+        learning_rate=1.0,
+        n_estimators=1,  # only one tree
+        n_jobs=8,
+        max_depth=6,  # maximum tree depth
+        random_state=seed,
+    )
     model.fit(X_train, y_train)
 
     return model
@@ -218,68 +210,72 @@ def evaluate(model, X, y):
     # compute and print fraction of correct labels
     # also measure time
 
-    print('success rate: ',
-          compute_identical_fraction(model.predict(X), y))
+    print("success rate: ", compute_identical_fraction(model.predict(X), y))
 
     start = time.time()
     model.predict(X)
     end = time.time()
-    print('n evaluations: ', X.shape[0])
-    print('elapsed time: ', end - start)
+    print("n evaluations: ", X.shape[0])
+    print("elapsed time: ", end - start)
 
 
-def load_data_and_fit_model(*,
-                            input_filename,
-                            output_filename,
-                            feature_names):
+def load_data_and_fit_model(*, input_filename, output_filename, feature_names):
 
-    print('preparing training data')
+    print("preparing training data")
     data = pd.read_pickle(input_filename)
     # %%
-    X_train, y_train, X_test, y_test = prepare_data(data=data,
-                                                    feature_names=feature_names,
-                                                    train_fraction=0.8,
-                                                    background_ratio=20,
-                                                    seed=22)
-    print('done preparing training data')
+    X_train, y_train, X_test, y_test = prepare_data(
+        data=data,
+        feature_names=feature_names,
+        train_fraction=0.8,
+        background_ratio=20,
+        seed=22,
+    )
+    print("done preparing training data")
 
-    print('fitting model')
+    print("fitting model")
     model = fit_model(X_train, y_train)
-    pickle.dump(model, open(output_filename, "wb"))
-    print('done fitting model')
+    #pickle.dump(model, open(output_filename, "wb"))
+    model.save_model(output_filename)
+    print("done fitting model")
 
-    print('test data ------------------------')
+    print("test data ------------------------")
     evaluate(model, X_test, y_test)
-    print('train data -----------------------')
+    print("train data -----------------------")
     evaluate(model, X_train, y_train)
 
 
-def load_model_and_generate_evaluation_images(*,
-                                              model_filename,
-                                              input_path,
-                                              output_path,
-                                              feature_names):
+def load_model_and_generate_evaluation_images(
+    *, model_filename, input_path, output_path, feature_names
+):
+    # TODO do I need to specify these arguments here when I load a model?
+    model = XGBClassifier(
+        learning_rate=1.0,
+        n_estimators=1,  # only one tree
+        n_jobs=8,
+        max_depth=6,  # maximum tree depth
+    )
+    model.load_model(model_filename)
+    #model = pickle.load(open(model_filename, "rb"))
 
-    model = pickle.load(open(model_filename, "rb"))
-
-    frame_folders = [f for f in os.listdir(
-        input_path) if os.path.isdir(os.path.join(input_path, f))]
-    frame_folders = sorted(frame_folders)
+    frame_folders = sorted(get_frame_folders(input_path))
 
     for frame_folder in frame_folders:
         frame_path = os.path.join(input_path, frame_folder)
-        segment_names = [name for name in os.listdir(
-            frame_path) if name[1].isdigit()]
+        segment_names = [
+            name for name in os.listdir(frame_path) if name[1].isdigit()
+        ]
 
         if len(segment_names) != 0:
             continue
 
-        for camera_name in ['60', '180', '300']:
-            image_name = 'camera' + camera_name + '.png'
-            print(frame_path + '/' + image_name)
+        for camera_name in ["60", "180", "300"]:
+            image_name = "camera" + camera_name + ".png"
+            print(frame_path + "/" + image_name)
             image_bgr = cv.imread(os.path.join(frame_path, image_name))
-            features, shape = create_features(image_bgr=image_bgr,
-                                              flatten=True)
+            features, shape = create_features(
+                image_bgr=image_bgr, flatten=True
+            )
 
             X = pd.DataFrame(features)[feature_names]
             y = model.predict(X)
@@ -294,39 +290,58 @@ def load_model_and_generate_evaluation_images(*,
                 os.makedirs(path)
 
             image_and_segments_bgr = np.concatenate(
-                [image_bgr, segments_bgr], axis=1)
+                [image_bgr, segments_bgr], axis=1
+            )
             # cv.imwrite(filename=os.path.join(path, image_name),
             #            img=image_bgr)
-            segments_filename = 'camera' + camera_name + '_segments' + '.png'
-            cv.imwrite(filename=os.path.join(path, segments_filename),
-                       img=image_and_segments_bgr)
+            segments_filename = "camera" + camera_name + "_segments" + ".png"
+            cv.imwrite(
+                filename=os.path.join(path, segments_filename),
+                img=image_and_segments_bgr,
+            )
 
 
 # %%
 if __name__ == "__main__":
-    try:
-        input_path = "./cube_image_set_p4_2020-09-16/"
-        output_path = "./output_xbg_all_features/"
 
-        # probably we could get rid of some of these features without
-        # losing much accuracy
-        feature_names = 'rgb' + 'hsv' + 'hls' + 'xyz' + 'LAB' + 'LUV'
-        # remove duplicates and sort for reproducibility
-        feature_names = sorted(list(set(feature_names)))
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--train",
+        action="store_true",
+        help="Train a new model.",
+    )
+    parser.add_argument(
+        "--image-dir",
+        required=True,
+        help="Directory containing the training data.",
+    )
+    parser.add_argument(
+        "--output-dir",
+        help="Output directory for test run.",
+    )
+    args = parser.parse_args()
 
-        load_images_and_create_data(input_path=input_path,
-                                    output_filename='data.pkl')
+    # probably we could get rid of some of these features without
+    # losing much accuracy
+    feature_names = "rgb" + "hsv" + "hls" + "xyz" + "LAB" + "LUV"
+    # remove duplicates and sort for reproducibility
+    feature_names = sorted(list(set(feature_names)))
 
-        load_data_and_fit_model(input_filename='data.pkl',
-                                output_filename="xgb_model.pkl",
-                                feature_names=feature_names)
+    if args.train:
+        load_images_and_create_data(
+            input_path=args.image_dir, output_filename="data.pkl"
+        )
 
-        load_model_and_generate_evaluation_images(model_filename="xgb_model.pkl",
-                                                  input_path=input_path,
-                                                  output_path=output_path,
-                                                  feature_names=feature_names)
+        load_data_and_fit_model(
+            input_filename="data.pkl",
+            output_filename="xgb_model.bin",
+            feature_names=feature_names,
+        )
 
-    except:
-        extype, value, tb = sys.exc_info()
-        traceback.print_exc()
-        ipdb.post_mortem(tb)
+    if args.output_dir:
+        load_model_and_generate_evaluation_images(
+            model_filename="xgb_model.bin",
+            input_path=args.image_dir,
+            output_path=args.output_dir,
+            feature_names=feature_names,
+        )
