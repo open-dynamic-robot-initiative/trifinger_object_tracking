f747a10183e59654b445a8685796f333209aa263
Manuel Wuthrich <manuel.wuthrich@gmail.com>
2020-10-08 00:18:36 +0200

    some peripheral stuff

--- run_shit.bash
+++ run_shit.bash
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0092
+# rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0107
+# rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0116
+# rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0117
+# rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0054
+# rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0058
+# rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0060
+# rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0064
+# rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0065
+
+
+
+
 for i in {001..158..1}
 do
     rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0$i
