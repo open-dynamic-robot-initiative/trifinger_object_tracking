0391b78d4acf0a6f4e7a6c3e5449d2839814d49f
Manuel Wuthrich <manuel.wuthrich@gmail.com>
2020-10-08 00:57:53 +0200

    generated results for new cube

--- run_shit.bash
+++ run_shit.bash
@@ -13,35 +13,70 @@
 
 
 
-for i in {001..158..1}
+# for i in {001..158..1}
+# do
+#     rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0$i
+# done
+
+# for i in {001..158..1}
+# do
+#     rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0$i
+# done
+
+
+# for i in {001..158..1}
+# do
+#     rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0$i
+# done
+
+
+# for i in {001..158..1}
+# do
+#     rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0$i
+# done
+
+
+# for i in {001..158..1}
+# do
+#     rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0$i
+# done
+
+# for i in {001..158..1}
+# do
+#     rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0$i
+# done
+
+
+
+for i in {001..80..1}
 do
-    rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0$i
+    rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/new_cube_image_set_2020-09-20/0$i
 done
 
-for i in {001..158..1}
+for i in {001..80..1}
 do
-    rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0$i
+    rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/new_cube_image_set_2020-09-20/0$i
 done
 
 
-for i in {001..158..1}
+for i in {001..80..1}
 do
-    rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0$i
+    rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/new_cube_image_set_2020-09-20/0$i
 done
 
 
-for i in {001..158..1}
+for i in {001..80..1}
 do
-    rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0$i
+    rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/new_cube_image_set_2020-09-20/0$i
 done
 
 
-for i in {001..158..1}
+for i in {001..80..1}
 do
-    rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0$i
+    rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/new_cube_image_set_2020-09-20/0$i
 done
 
-for i in {001..158..1}
+for i in {001..80..1}
 do
-    rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/cube_image_set_p4_2020-09-16/0$i
+    rosrun trifinger_object_tracking single_observation $(rospack find trifinger_object_tracking)/data/new_cube_image_set_2020-09-20/0$i
 done
