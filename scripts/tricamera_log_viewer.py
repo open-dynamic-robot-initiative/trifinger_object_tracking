0b17414d8ecdc05299b1f220f154a1e28887a701
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-11-06 13:53:20 +0100

    tricamera_log_viewer: Visualise goal from goal.json
    Add an option to tricamera_log_viewer.py to visualise the goal pose from
    a specified goal.json file.

--- scripts/tricamera_log_viewer.py
+++ scripts/tricamera_log_viewer.py
@@ -3,7 +3,7 @@
 Play back TriCameraObjectObservations from a log file.
 """
 import argparse
-import copy
+import json
 import pathlib
 import sys
 
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 
 import trifinger_cameras
+import trifinger_object_tracking.py_object_tracker
 import trifinger_object_tracking.py_tricamera_types as tricamera
 from trifinger_cameras import utils
 
@@ -34,6 +35,13 @@ def main():
         camera{60,180,300}.yml with calibration parameters to exist in the same
         directory as the given camera log file.
         """,
+    )
+    parser.add_argument(
+        "--visualize-goal-pose",
+        "-g",
+        type=pathlib.Path,
+        metavar="GOAL_FILE",
+        help="Visualize goal from the specified JSON file.",
     )
     parser.add_argument(
         "--show-confidence",
@@ -64,8 +72,20 @@ def main():
         print("{} does not exist.".format(log_file_path))
         sys.exit(1)
 
+    if args.visualize_goal_pose:
+        if not args.visualize_goal_pose.exists():
+            print("{} does not exist.".format(args.visualize_goal_pose))
+            sys.exit(1)
+
+        with open(args.visualize_goal_pose, "r") as fh:
+            goal_dict = json.load(fh)
+
+        goal_pose = trifinger_object_tracking.py_object_tracker.ObjectPose()
+        goal_pose.position = goal_dict["goal"]["position"]
+        goal_pose.orientation = goal_dict["goal"]["orientation"]
+
     calib_files = []
-    if args.visualize_object_pose:
+    if args.visualize_object_pose or args.visualize_goal_pose:
         for name in ("camera60", "camera180", "camera300"):
             calib_file = log_file_path.parent / (name + ".yml")
             if calib_file.exists():
@@ -99,8 +119,8 @@ def main():
             object_pose = observation.filtered_object_pose
 
         if args.compensate_cube_offset:
-            # object_pose is read-only and unforutnatly copy.copy does not work
-            # for this type
+            # object_pose is read-only and unfortunately copy.copy does not
+            # work for this type
             object_pose_cpy = type(object_pose)()
             object_pose_cpy.position = np.array(object_pose.position)
             object_pose_cpy.orientation = np.array(object_pose.orientation)
@@ -110,6 +130,11 @@ def main():
             offset = np.array([0, 0, 0.0325])
             cube_rot = Rotation.from_quat(object_pose.orientation)
             object_pose.position = object_pose.position + cube_rot.apply(offset)
+
+        if args.visualize_goal_pose:
+            cvmats = [trifinger_cameras.camera.cvMat(img) for img in images]
+            images = cube_visualizer.draw_cube(cvmats, goal_pose, True)
+            images = [np.array(img) for img in images]
 
         if args.visualize_object_pose:
             cvmats = [trifinger_cameras.camera.cvMat(img) for img in images]
