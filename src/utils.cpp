10dde7466e536dc130f08776c01766dcf9cf2299
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-11-25 11:53:23 +0100

    Refactor load_camera_parameters
    - Separate error message for failure while loading parameters and camera
      name mismatch.
    - Use a loop to avoid code duplication.
    - Remove unused includes.

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,43 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <algorithm>
-#include <armadillo>
-#include <boost/algorithm/string.hpp>
+/**
+ * @file
+ * @copyright 2020, Max Planck Gesellschaft.  All rights reserved.
+ * @license BSD 3-clause
+ */
 #include <trifinger_object_tracking/utils.hpp>
+
 #include <trifinger_cameras/parse_yml.h>
-
-
-namespace fs = boost::filesystem;
 
 namespace trifinger_object_tracking
 {
 std::array<trifinger_cameras::CameraParameters, 3> load_camera_parameters(
     std::array<std::string, 3> parameter_files)
 {
+    static const std::string camera_names[3] = {
+        "camera60", "camera180", "camera300"};
+
     std::array<trifinger_cameras::CameraParameters, 3> camera_params;
-    std::string camera_name;
-    bool success;
 
-    success = trifinger_cameras::readCalibrationYml(
-        parameter_files[0], camera_name, camera_params[0]);
-    if (!success || camera_name != "camera60")
+    for (size_t i = 0; i < 3; i++)
     {
-        throw std::runtime_error("Failed to load parameters of camera60 ");
-    }
+        bool success;
+        std::string camera_name;
 
-    success = trifinger_cameras::readCalibrationYml(
-        parameter_files[1], camera_name, camera_params[1]);
-    if (!success || camera_name != "camera180")
-    {
-        throw std::runtime_error("Failed to load parameters of camera180");
-    }
-
-    success = trifinger_cameras::readCalibrationYml(
-        parameter_files[2], camera_name, camera_params[2]);
-    if (!success || camera_name != "camera300")
-    {
-        throw std::runtime_error("Failed to load parameters of camera300");
+        success = trifinger_cameras::readCalibrationYml(
+            parameter_files[i], camera_name, camera_params[i]);
+        if (!success)
+        {
+            throw std::runtime_error("Failed to load parameters of " +
+                                     camera_names[i]);
+        }
+        if (camera_name != camera_names[i])
+        {
+            throw std::runtime_error(
+                "Camera name in calibration file does not match with " +
+                camera_names[i]);
+        }
     }
 
     return camera_params;
