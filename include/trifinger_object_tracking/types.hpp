42d1ae96f3f343973f6900f6b0bfae47022217d4
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-11-25 17:18:38 +0100

    Clean-up unused code

--- include/trifinger_object_tracking/types.hpp
+++ include/trifinger_object_tracking/types.hpp
@@ -11,26 +11,6 @@
 
 namespace trifinger_object_tracking
 {
-struct ColorBounds
-{
-    int lower[3];
-    int upper[3];
-};
-
-struct Line
-{
-    float a;
-    float b;
-
-    Line() : a(0.0), b(0.0)
-    {
-    }
-
-    Line(float a, float b) : a(a), b(b)
-    {
-    }
-};
-
 struct Pose
 {
     // TODO use non-cv types?
@@ -44,15 +24,9 @@ struct Pose
     Pose(const cv::Vec3f &translation,
          const cv::Vec3f &rotation,
          float confidence = 0.0)
-        : translation(translation),
-          rotation(rotation),
-          confidence(confidence)
+        : translation(translation), rotation(rotation), confidence(confidence)
     {
     }
 };
-
-typedef std::pair<FaceColor, FaceColor> ColorPair;
-
-typedef std::vector<std::pair<ColorPair, Line>> ColorEdgeLineList;
 
 }  // namespace trifinger_object_tracking
