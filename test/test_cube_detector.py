ebc24fdb19273e2c36fab703cf1832124c4d8ee2
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-03-18 17:45:32 +0100

    test: Check orientation in Python tests
    In Python this is very easy with scipy, so do that here.

--- test/test_cube_detector.py
+++ test/test_cube_detector.py
@@ -1,10 +1,15 @@
 import pathlib
 
-import pytest
 import cv2
 import numpy as np
+from scipy.spatial.transform import Rotation
 
 import trifinger_object_tracking.py_object_tracker as object_tracker
+
+
+def rotation_error(actual: Rotation, expected: Rotation) -> float:
+    error_rot = expected.inv() * actual
+    return error_rot.magnitude()
 
 
 def load_test_data(object_name):
@@ -36,15 +41,15 @@ def test_find_pose_cube_v1():
 
     # TODO the detected position is actually a bit off here.  The cube is
     # on the ground, so the height would need to be 0.0325.
-    actual_position = np.array([-0.014, -0.024, 0.028])
-    actual_orientation = np.array(
+    expected_position = np.array([-0.014, -0.024, 0.028])
+    expected_orientation = Rotation.from_quat(
         [0.58214203, 0.36977536, -0.61211132, 0.38690641]
     )
 
-    np.testing.assert_array_almost_equal(pose.position, actual_position, 2)
+    np.testing.assert_array_almost_equal(pose.position, expected_position, 2)
 
-    # TODO What is a good way to compare orientation vectors?
-    # np.testing.assert_array_almost_equal(pose.orientation, actual_orientation, 2)
+    actual_orientation = Rotation.from_quat(pose.orientation)
+    assert rotation_error(actual_orientation, expected_orientation) < 0.1
 
     assert pose.confidence > 0.8
 
@@ -61,15 +66,15 @@ def test_find_pose_cube_v2():
 
     # TODO the detected position is actually a bit off here.  The cube is
     # on the ground, so the height would need to be 0.0325.
-    actual_position = np.array([-0.063, -0.136, 0.025])
-    actual_orientation = np.array(
+    expected_position = np.array([-0.063, -0.136, 0.025])
+    expected_orientation = Rotation.from_quat(
         [0.1192724, 0.6887785, -0.1423320, 0.7007851]
     )
 
-    np.testing.assert_array_almost_equal(pose.position, actual_position, 2)
+    np.testing.assert_array_almost_equal(pose.position, expected_position, 2)
 
-    # TODO What is a good way to compare orientation vectors?
-    # np.testing.assert_array_almost_equal(pose.orientation, actual_orientation, 2)
+    actual_orientation = Rotation.from_quat(pose.orientation)
+    assert rotation_error(actual_orientation, expected_orientation) < 0.1
 
     assert pose.confidence > 0.8
 
@@ -86,14 +91,15 @@ def test_find_pose_cuboid_2x2x8_v2():
 
     # TODO the detected position is actually a bit off here.  The cube is
     # on the ground, so the height would need to be 0.0325.
-    actual_position = np.array([-0.007, -0.008, 0.04])
-    actual_orientation = np.array(
+    expected_position = np.array([-0.007, -0.008, 0.04])
+    expected_orientation = Rotation.from_quat(
         [0.67863974, -0.148312, -0.1528063, 0.70292382]
     )
 
-    np.testing.assert_array_almost_equal(pose.position, actual_position, 2)
+    np.testing.assert_array_almost_equal(pose.position, expected_position, 2)
 
-    # TODO What is a good way to compare orientation vectors?
-    # np.testing.assert_array_almost_equal(pose.orientation, actual_orientation, 2)
+    # orientation for the small cuboid can be very off, so don't check this here
+    # actual_orientation = Rotation.from_quat(pose.orientation)
+    # assert rotation_error(actual_orientation, expected_orientation) < 0.1
 
     assert pose.confidence > 0.8
