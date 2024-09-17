d063a3dfbdc89faf09e9bed423ddbab7b46934e1
Felix Kloss <felix.kloss@uni-tuebingen.de>
2024-09-17 11:56:43 +0200

    Add backend-only mode to demo_cameras
    Extend demo_cameras a bit by adding a --backend-only flag.  If set, only
    the back end is run, using multi-process data.
    The former `--multi-process` flag has been renamed to `--frontend-only`
    for consistency.
    
    With this change, the script can be used to run the camera backend
    independent of any robot-stuff.  Actually it should probably be promoted
    from a demo to a full-fledged script.

--- demos/demo_cameras.py
+++ demos/demo_cameras.py
@@ -2,66 +2,44 @@
 """Demo on how to access observations from the TriCameraObjectTrackerDriver."""
 
 import argparse
+import sys
 import time
 
 import cv2
 import numpy as np
 
-import trifinger_object_tracking.py_object_tracker
+import signal_handler
+import trifinger_object_tracking.py_object_tracker as object_tracker
 import trifinger_object_tracking.py_tricamera_types as tricamera
 from trifinger_cameras import utils
 
 
-def main() -> None:  # noqa: D103
-    argparser = argparse.ArgumentParser(description=__doc__)
-    argparser.add_argument(
-        "--multi-process",
-        action="store_true",
-        help="""If set, use multiprocess camera data to access backend in other
-            process.  Otherwise run the backend locally.
-        """,
-    )
-    argparser.add_argument(
-        "--object",
-        type=str,
-        default="cube_v2",
-        help="Name of the model used for object detection.  Default: %(default)s.",
-    )
-    args = argparser.parse_args()
+camera_names = ["camera60", "camera180", "camera300"]
+calib_files = [
+    "/etc/trifingerpro/camera60_cropped_and_downsampled.yml",
+    "/etc/trifingerpro/camera180_cropped_and_downsampled.yml",
+    "/etc/trifingerpro/camera300_cropped_and_downsampled.yml",
+]
 
-    camera_names = ["camera60", "camera180", "camera300"]
 
-    model = trifinger_object_tracking.py_object_tracker.get_model_by_name(args.object)
+def back_end_only_loop() -> None:
+    """Idle loop for the back-end-only mode."""
+    print("Backend is running.  Press Ctrl-C to stop.")
+    signal_handler.init()
+    while not signal_handler.has_received_sigint():
+        time.sleep(1)
 
-    if args.multi_process:
-        camera_data = tricamera.MultiProcessData("tricamera", False)
-    else:
-        camera_data = tricamera.SingleProcessData()
-        camera_driver = tricamera.TriCameraObjectTrackerDriver(
-            *camera_names, cube_model=model
-        )
-        camera_backend = tricamera.Backend(camera_driver, camera_data)
 
+def front_end_loop(
+    camera_data: tricamera.BaseData, model: object_tracker.BaseCuboidModel
+) -> None:
+    """Run the front end to display camera images with visualized object pose."""
     camera_frontend = tricamera.Frontend(camera_data)
 
-    calib_files = [
-        "/etc/trifingerpro/camera60_cropped_and_downsampled.yml",
-        "/etc/trifingerpro/camera180_cropped_and_downsampled.yml",
-        "/etc/trifingerpro/camera300_cropped_and_downsampled.yml",
-    ]
     cube_visualizer = tricamera.CubeVisualizer(model, calib_files)
 
-    np.set_printoptions(precision=3, suppress=True)
-    print("=== Camera Info: ======================")
-    for i, info in enumerate(camera_frontend.get_sensor_info().camera):
-        print(f"--- Camera {i}: ----------------------")
-        print(f"fps: {info.frame_rate_fps}")
-        print(f"width x height: {info.image_width}x{info.image_height}")
-        print(info.camera_matrix)
-        print(info.distortion_coefficients)
-        print(info.tf_world_to_camera)
-        print("---------------------------------------")
-    print("=======================================")
+    print("=== Camera Info: ===")
+    utils.print_tricamera_sensor_info(camera_frontend.get_sensor_info())
 
     last_print = time.time()
     while True:
@@ -84,9 +62,71 @@ def main() -> None:  # noqa: D103
         if cv2.waitKey(3) in [ord("q"), 27]:  # 27 = ESC
             break
 
-    if not args.multi_process:
+
+def main() -> int:  # noqa: D103
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    mode_group = parser.add_mutually_exclusive_group()
+    mode_group.add_argument(
+        "--frontend-only",
+        action="store_const",
+        dest="mode",
+        const="frontend-only",
+        help="""Run only the front end, using multi-process camera data.  In this case,
+            the back end has to be run in a separate process.
+        """,
+    )
+    mode_group.add_argument(
+        "--backend-only",
+        action="store_const",
+        dest="mode",
+        const="backend-only",
+        help="""Run only the back end, using multi-process camera data.  In this case,
+            the front end has to be run in a separate process.
+        """,
+    )
+    mode_group.add_argument(
+        "--single-process",
+        action="store_const",
+        dest="mode",
+        const="single-process",
+        help="Run both front and back end, using single-process camera data.",
+    )
+    mode_group.set_defaults(mode="single-process")
+
+    parser.add_argument(
+        "--object",
+        type=str,
+        default="cube_v2",
+        help="Name of the model used for object detection.  Default: %(default)s.",
+    )
+    args = parser.parse_args()
+
+    model = object_tracker.get_model_by_name(args.object)
+
+    if args.mode == "single-process":
+        camera_data = tricamera.SingleProcessData()
+    else:
+        camera_data = tricamera.MultiProcessData("tricamera", False)
+
+    if args.mode != "frontend-only":
+        camera_driver = tricamera.TriCameraObjectTrackerDriver(
+            *camera_names, cube_model=model
+        )
+        camera_backend = tricamera.Backend(camera_driver, camera_data)
+    else:
+        camera_backend = None
+
+    if args.mode == "backend-only":
+        back_end_only_loop()
+    else:
+        front_end_loop(camera_data, model)
+
+    if camera_backend is not None:
         camera_backend.shutdown()
+
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
