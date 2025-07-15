71a91b2491b53a5e621d4ae225c6380af7bf2c3d
Felix Kloss <felix.kloss@uni-tuebingen.de>
2025-07-15 18:11:16 +0200

    Some fixes in demo_tricamera_separate_backend.py
    Start loggers before backends, so no data is missed.
    Shutdown tracker backend before camera backend, otherwise the former blocks.

--- demos/demo_tricamera_separate_backend.py
+++ demos/demo_tricamera_separate_backend.py
@@ -23,34 +23,37 @@ def main() -> int:
         format="[%(asctime)s] [%(name)s | %(levelname)s] %(message)s",
     )
 
-    # create tricamera backend
     camera_data = tc.tricamera.SingleProcessData()
+    tracker_data = ot_tricamera.SingleProcessData()
+
+    logging.debug("create and start loggers")
+    camera_logger = tc.tricamera.Logger(camera_data, 1000)
+    tracker_logger = ot_tricamera.Logger(tracker_data, 1000)
+    camera_logger.start()
+    tracker_logger.start()
+
+    logging.debug("create tricamera backend")
     camera_driver = tc.tricamera.TriCameraDriver(*tc.CAMERA_NAMES)
     camera_backend = tc.tricamera.Backend(camera_driver, camera_data)
 
-    # create tricameraobjecttracking driver & backend
+    logging.debug("create tricameraobjecttracking driver & backend")
     model = object_tracker.get_model_by_name("cube_v2")
-    tracker_data = ot_tricamera.SingleProcessData()
     tracker_driver = ot_tricamera.TriCameraObjectTrackerDriver(
         camera_data, cube_model=model
     )
     tracker_backend = ot_tricamera.Backend(tracker_driver, tracker_data)
 
-    # create loggers for both
-    camera_logger = tc.tricamera.Logger(camera_data, buffer_size=1000)
-    tracker_logger = ot_tricamera.Logger(tracker_data, buffer_size=1000)
-
     # run until logger buffers are full and save to /tmp
-    camera_logger.start()
-    tracker_logger.start()
     while camera_logger.is_running() or tracker_logger.is_running():
         time.sleep(0.1)
 
+    logging.info("Finished logging.  Write data to /tmp.")
     camera_logger.stop_and_save("/tmp/camera_log.dat")
     tracker_logger.stop_and_save("/tmp/tracker_log.dat")
 
-    camera_backend.shutdown()
+    logging.info("Shutdown the backends")
     tracker_backend.shutdown()
+    camera_backend.shutdown()
 
     return 0
 
